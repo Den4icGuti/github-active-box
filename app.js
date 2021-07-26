a9f68983a243e1b030f45e4b4cda65f4b460efee
Den4icGuti <goos7412@gmail.com>
2021-07-26 20:15:11 +0300

    Подключен Js,дабвлен header posion fixsed, при скролленге

---
+++ app.js
@@ -0,0 +1,27 @@
+$(function () {
+    let header = $("#header");
+
+    let intro = $("#intro");
+    
+    let introH;
+  
+
+    let scrollPos = $(window).scrollTop();
+
+    
+    
+    $(window).on("scroll load", function () {
+          let introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+        if (scrollPos > introH) {
+             header.addClass("fixed");
+        } else {
+             header.removeClass("fixed");
+        }
+      
+    });
+
+    header.addClass("fixsed");
+
+
+});
